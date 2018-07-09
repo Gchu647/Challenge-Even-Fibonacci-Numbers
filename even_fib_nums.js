5e1f9838ea5195cf3530243488c34922836ec439
George <quicksilver-ga@hotmail.com>
2018-07-09 10:33:31 -1000

    Pass all 4 tests

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -9,6 +9,20 @@ function _sumFibs( maxFibValue ) {
   var sum = 0;
 
   // do your work here
+  let fibArr = [1, 2];
+
+  for(let i = 2; i < maxFibValue; i++) {
+    let newNum = fibArr[i-1] + fibArr[i-2]; // The newNum is always the sum of the previous two numbers
+    fibArr.push(newNum);
+  }
+  
+  for(let i = 0; fibArr[i] <= maxFibValue; i++) {
+    if(fibArr[i]%2 === 0) {
+      sum += fibArr[i]; //Adds fibArr index value to the sum during each iteration.
+    }
+  }
+
+  console.log(sum);
 
   return sum;
 }
@@ -18,9 +32,24 @@ function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
 
   //define your base case, validate your input
+  let fibArr = [1, 2];
 
+  for(let i = 2; i < maxFibValue; i++) {
+    let newNum = fibArr[i-1] + fibArr[i-2]; // The newNum is always the sum of the previous two numbers
+    fibArr.push(newNum);
+  }
 
   //do your work here
+  function stopAtMax(maxFibValue) {
+    for(let i = fibArr.length - 1; i >= 0; i--) {
+      if(fibArr[i] < maxFibValue) {
+        let maxNum = fibArr[i];
+        return maxNum;
+      }
+    }
+  }
+
+  highest = stopAtMax(maxFibValue);
 
   return highest;
 };
@@ -33,3 +62,5 @@ module.exports = {
   sumFibs : _sumFibs,
   highestFibonacciNumber : _highestFibonacciNumber
 };
+
+//Fib sequence: 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
